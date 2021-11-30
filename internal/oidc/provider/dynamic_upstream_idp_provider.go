83082b868392f91d3b0485cfed93672370a5f695
Margo Crawford <margaretc@vmware.com>
2021-11-29 16:44:58 -0800

    Check for subject matching with upstream refresh
    Signed-off-by: Margo Crawford <margaretc@vmware.com>

--- internal/oidc/provider/dynamic_upstream_idp_provider.go
+++ internal/oidc/provider/dynamic_upstream_idp_provider.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"net/url"
 	"sync"
+	"time"
 
 	"golang.org/x/oauth2"
 	"k8s.io/apimachinery/pkg/types"
@@ -94,6 +95,13 @@ type UpstreamLDAPIdentityProviderI interface {
 
 	// PerformRefresh performs a refresh against the upstream LDAP identity provider
 	PerformRefresh(ctx context.Context, userDN, expectedUsername, expectedSubject string) error
+}
+
+type StoredRefreshAttributes struct {
+	Username string
+	Subject  string
+	DN       string
+	AuthTime time.Time
 }
 
 type DynamicUpstreamIDPProvider interface {
